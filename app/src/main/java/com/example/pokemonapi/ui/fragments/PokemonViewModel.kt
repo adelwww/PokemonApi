2ea58b90b6b772eca2ce11280f7bc0173cdeb792
adelwww <wsnezka@gmail.com>
2022-08-10 19:17:42 +0300

    add viewModels

---
+++ app/src/main/java/com/example/pokemonapi/ui/fragments/PokemonViewModel.kt
@@ -0,0 +1,11 @@
+package com.example.pokemonapi.ui.fragments
+
+import com.example.pokemonapi.ui.base.BaseViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class PokemonViewModel @Inject constructor(
+
+) : BaseViewModel() {
+}
